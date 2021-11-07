c0ab2c8e046101ff203ffc5cfd67ddff2d47674d
seosd <seosd97@gmail.com>
2021-11-08 08:17:47 +0900

    create src file directory

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export enum MatchCategoryType {
+  NORMAL = 'Normal',
+  SOLORANK = 'SoloRank',
+  FREERANK = 'FreeRank',
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>your.gg interview task</title>
+        <meta name="description" content="your.gg interview task - hide on bush stat" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <b>HIDE ON BUSH</b>
+      </main>
+    </div>
+  )
+}
+
+export default Home
